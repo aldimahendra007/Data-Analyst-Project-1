1c9c565914b4f382266e47982b2eb39f074d481b
Aldi Mahendra <91265505+aldimahendra007@users.noreply.github.com>
2023-11-15 10:40:32 +0700

    Delete Business Questions and Answers directory

--- Business Questions and Answers/Question 1/Extracting the data needed.sql
+++
@@ -1,9 +0,0 @@
-select t1.orderDate, t1.orderNumber, t3.productName, 
-t3.productLine, t4.city, t4.country, t2.quantityOrdered, t2.priceEach
-from orders t1
-join orderdetails t2
-on t1.orderNumber = t2.orderNumber
-join products t3
-on t2.productCode = t3.productCode
-join customers t4
-on t1.customerNumber = t4.customerNumber
